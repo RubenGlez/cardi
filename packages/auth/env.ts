3891c490f2a1a7d7e7e47a81d85e7c715ccfd9df
RubenGlez <ruben.daren@gmail.com>
2024-07-16 20:09:37 +0200

    chore: finishing repo setup

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -1,6 +1,14 @@
 /* eslint-disable no-restricted-properties */
-import { createEnv } from "@t3-oss/env-nextjs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createEnv } from "@t3-oss/env-core";
+import dotenv from "dotenv";
 import { z } from "zod";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const envPath = path.resolve(__dirname, "../../.env");
+dotenv.config({ path: envPath });
 
 export const env = createEnv({
   server: {
@@ -9,6 +17,11 @@ export const env = createEnv({
     AUTH_API_REFRESH_TOKEN_SECRET: z.string().min(1),
   },
   client: {},
-  experimental__runtimeEnv: {},
+  runtimeEnvStrict: {
+    AUTH_API_PORT: Number(process.env.AUTH_API_PORT),
+    AUTH_API_ACCESS_TOKEN_SECRET: process.env.AUTH_API_ACCESS_TOKEN_SECRET,
+    AUTH_API_REFRESH_TOKEN_SECRET: process.env.AUTH_API_REFRESH_TOKEN_SECRET,
+  },
+  clientPrefix: "",
   skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
 });
